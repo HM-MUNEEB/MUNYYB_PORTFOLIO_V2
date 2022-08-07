addff391c8e7fe0745faa3b110929962cc612576
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-08-07 12:55:46 -0700

    GSAP: Animtion on both component mount and component original position

--- Components/Container/Container.js
+++ Components/Container/Container.js
@@ -2,24 +2,38 @@ import styles from "./Container.module.css";
 import { useState, useEffect, useRef } from "react";
 import InitialIntro from "../Initial-Intro/InitialIntro";
 import LogoContainer from "../Logo-Container/LogoContainer";
+import { gsap } from "gsap";
 
 export default function Container() {
   const [isInitialIntro, setIsInitialIntro] = useState(true);
+  const InitialIntroRed = useRef();
 
   setInterval(() => {
     setIsInitialIntro(false);
-  }, 25000);
+    gsap.fromTo(
+      InitialIntroRed.current,
+      {
+        opacity: 0,
+        y: -25,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+      }
+    );
+  }, 5000);
+
+  useEffect(() => {}, []);
   return (
     <div className={styles.MunyybContainer}>
-      <div className={styles.IntroAndLogoContainer}>
-        {isInitialIntro ? (
-          <div className={styles.typicalTextAnimation}>
-            <InitialIntro />
-          </div>
-        ) : (
-          <LogoContainer />
-        )}
-      </div>
+      {isInitialIntro ? (
+        <div className={styles.typicalTextAnimation} ref={InitialIntroRed}>
+          <InitialIntro />
+        </div>
+      ) : (
+        <LogoContainer />
+      )}
     </div>
   );
 }
--- Components/Logo-Container/LogoContainer.js
+++ Components/Logo-Container/LogoContainer.js
@@ -7,16 +7,35 @@ import { gsap } from "gsap";
 export default function LogoContainer() {
   const LogoContainerRef = useRef();
   useEffect(() => {
+    var h = window.innerHeight / 2 - 10;
+    var w = window.innerWidth / 2 - 100;
+    //Fading In transition
     gsap.fromTo(
       LogoContainerRef.current,
       {
         opacity: 0,
-        y: -25,
+        y: h - 25,
       },
       {
         opacity: 1,
-        y: 0,
+        y: h,
         duration: 0.5,
+      }
+    );
+    //Placing logo to original position
+    gsap.fromTo(
+      LogoContainerRef.current,
+      {
+        opacity: 1,
+        y: h,
+        x: w,
+      },
+      {
+        y: 0,
+        x: 0,
+        delay: 2,
+        duration: 0.5,
+        ease: "back.easeOut",
       }
     );
   }, []);
