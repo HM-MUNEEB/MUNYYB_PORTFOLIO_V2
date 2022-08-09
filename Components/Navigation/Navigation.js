d83711065f75d4770b8f5424907ec1afcf9d8b83
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-08-09 12:03:27 -0700

    Navigation Items color setup on defualt and hover

--- Components/Navigation/Navigation.js
+++ Components/Navigation/Navigation.js
@@ -3,15 +3,21 @@ import styles from "./Navigation.module.css";
 export default function Navigation() {
   return (
     <div className={styles.Navigation}>
-      <ul className={styles.NavItems}>
-        <li>
-          <h1>Work</h1>
+      <ul className={styles.NavItems_container}>
+        <li className={styles.NavItem}>
+          <a className={styles.Main_Nav}>
+            <h1 className={styles.Nav_Heading_1}>Work</h1>
+          </a>
         </li>
-        <li>
-          <h1>About</h1>
+        <li className={styles.NavItem}>
+          <a className={styles.Main_Nav}>
+            <h1 className={styles.Nav_Heading_1}>About</h1>
+          </a>
         </li>
-        <li>
-          <h1>Contact Me</h1>
+        <li className={styles.NavItem}>
+          <a className={styles.Main_Nav}>
+            <h1 className={styles.Nav_Heading_1}>Contact Me</h1>
+          </a>
         </li>
       </ul>
     </div>
