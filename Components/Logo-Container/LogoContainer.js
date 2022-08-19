36d0266893b90af334b92390b175afc2daf2065a
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-08-19 23:42:12 +0500

    Font adjustment to view-port size

--- Components/Logo-Container/LogoContainer.js
+++ Components/Logo-Container/LogoContainer.js
@@ -5,43 +5,12 @@ import { useState, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
 export default function LogoContainer() {
-  // useEffect(() => {
-  //   var h = window.innerHeight / 2 - 10;
-  //   var w = window.innerWidth / 2 - 100;
-  //   //Fading In transition
-  //   gsap.fromTo(
-  //     LogoContainerRef.current,
-  //     {
-  //       opacity: 0,
-  //       y: h - 25,
-  //     },
-  //     {
-  //       opacity: 1,
-  //       y: h,
-  //       duration: 0.5,
-  //     }
-  //   );
-  //   //Placing logo to original position
-  //   gsap.fromTo(
-  //     LogoContainerRef.current,
-  //     {
-  //       opacity: 1,
-  //       y: h,
-  //       x: w,
-  //     },
-  //     {
-  //       y: 0,
-  //       x: 0,
-  //       delay: 2,
-  //       duration: 0.5,
-  //       ease: "back.easeOut",
-  //     }
-  //   );
-  // }, []);
   return (
     <header className={styles.Header}>
       <div className={styles.LogoContainer}>
-        <Image src={LogoImg} width={40} height={40} />
+        <div className={styles.LogoImgContainer}>
+          <Image src={LogoImg} className={styles.LogoImg} />
+        </div>
         <h1>MUNYYB.</h1>
       </div>
     </header>
