c997a9ee3d8d8d91ff056c0972cacd2984cfbb6b
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-08-25 23:47:20 +0500

    Went for a different direction for the background

--- Components/Background/BackgroundCanvas.js
+++ Components/Background/BackgroundCanvas.js
@@ -1,50 +1,153 @@
-import styles from "./BackgroundCanvas.module.css";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect, useRef, useState } from "react";
+import useInterval from "@use-it/interval";
 
-export default function BackgroundCanvas() {
-  // Initialising the canvas
-  var canvas = document.querySelector("canvas"),
-    ctx = canvas.getContext("2d");
+// Constants
+const VALID_CHARS = `abcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\\|{}<>[]^~`;
+const STREAM_MUTATION_ODDS = 0.02;
 
-  // Setting the width and height of the canvas
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+const MIN_STREAM_SIZE = 15;
+const MAX_STREAM_SIZE = 50;
 
-  // Setting up the letters
-  var letters =
-    "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ";
-  letters = letters.split("");
+const MIN_INTERVAL_DELAY = 50;
+const MAX_INTERVAL_DELAY = 100;
 
-  // Setting up the columns
-  var fontSize = 10,
-    columns = canvas.width / fontSize;
+const MIN_DELAY_BETWEEN_STREAMS = 0;
+const MAX_DELAY_BETWEEN_STREAMS = 8000;
 
-  // Setting up the drops
-  var drops = [];
-  for (var i = 0; i < columns; i++) {
-    drops[i] = 1;
-  }
+const getRandInRange = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min;
 
-  // Setting up the draw function
-  function draw() {
-    ctx.fillStyle = "rgba(0, 0, 0, .1)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < drops.length; i++) {
-      var text = letters[Math.floor(Math.random() * letters.length)];
-      ctx.fillStyle = "#0f0";
-      ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-      drops[i]++;
-      if (drops[i] * fontSize > canvas.height && Math.random() > 0.95) {
-        drops[i] = 0;
-      }
+const getRandChar = () =>
+  VALID_CHARS.charAt(Math.floor(Math.random() * VALID_CHARS.length));
+
+const getRandStream = () =>
+  new Array(getRandInRange(MIN_STREAM_SIZE, MAX_STREAM_SIZE))
+    .fill()
+    .map((_) => getRandChar());
+
+const getMutatedStream = (stream) => {
+  const newStream = [];
+  for (let i = 1; i < stream.length; i++) {
+    if (Math.random() < STREAM_MUTATION_ODDS) {
+      newStream.push(getRandChar());
+    } else {
+      newStream.push(stream[i]);
     }
   }
+  newStream.push(getRandChar());
+  return newStream;
+};
 
-  // Loop the animation
-  setInterval(draw, 33);
+const RainStream = (props) => {
+  const [stream, setStream] = useState(getRandStream());
+  const [topPadding, setTopPadding] = useState(stream.length * -50);
+  const [intervalDelay, setIntervalDelay] = useState(null);
+
+  // Initialize intervalDelay
+  useEffect(() => {
+    setTimeout(() => {
+      setIntervalDelay(getRandInRange(MIN_INTERVAL_DELAY, MAX_INTERVAL_DELAY));
+    }, getRandInRange(MIN_DELAY_BETWEEN_STREAMS, MAX_DELAY_BETWEEN_STREAMS));
+  }, []);
+
+  useInterval(() => {
+    if (!props.height) return;
+
+    if (!intervalDelay) return;
+
+    // If stream is off the screen, reset it after timeout
+    if (topPadding > props.height) {
+      setStream([]);
+      const newStream = getRandStream();
+      setStream(newStream);
+      setTopPadding(newStream.length * -44);
+      setIntervalDelay(null);
+      setTimeout(
+        () =>
+          setIntervalDelay(
+            getRandInRange(MIN_INTERVAL_DELAY, MAX_INTERVAL_DELAY)
+          ),
+        getRandInRange(MIN_DELAY_BETWEEN_STREAMS, MAX_DELAY_BETWEEN_STREAMS)
+      );
+    } else {
+      setTopPadding(topPadding + 44);
+    }
+    // setStream(stream => [...stream.slice(1, stream.length), getRandChar()]);
+    setStream(getMutatedStream);
+  }, intervalDelay);
+
   return (
-    <div className={styles.BackgroundCanvas}>
-      <h1>hello world</h1>
-      <canvas></canvas>
+    <div
+      style={{
+        fontFamily: "matrixFont",
+        color: "#20c20e",
+        writingMode: "vertical-rl",
+        textOrientation: "upright",
+        userSelect: "none",
+        whiteSpace: "nowrap",
+        marginTop: topPadding,
+        marginLeft: -15,
+        marginRight: -15,
+        textShadow: "0px 0px 8px rgba(32, 194, 14, 0.8)",
+        fontSize: 50,
+      }}
+    >
+      {stream.map((char, index) => (
+        <a
+          style={{
+            marginTop: -12,
+            // Reduce opacity for last chars
+            opacity: index < 6 ? 0.1 + index * 0.15 : 1,
+            color: index === stream.length - 1 ? "#fff" : undefined,
+            textShadow:
+              index === stream.length - 1
+                ? "0px 0px 20px rgba(255, 255, 255, 1)"
+                : undefined,
+          }}
+        >
+          {char}
+        </a>
+      ))}
     </div>
   );
-}
+};
+
+const MatrixRain = (props) => {
+  const containerRef = useRef(null);
+  const [containerSize, setContainerSize] = useState(null); // ?{width, height}
+
+  useEffect(() => {
+    const boundingClientRect = containerRef.current.getBoundingClientRect();
+    setContainerSize({
+      width: boundingClientRect.width,
+      height: boundingClientRect.height,
+    });
+  }, []);
+
+  const streamCount = containerSize ? Math.floor(containerSize.width / 26) : 0;
+
+  return (
+    <div
+      style={{
+        background: "black",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        overflow: "ignore",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+      ref={containerRef}
+    >
+      {new Array(streamCount).fill().map((_) => (
+        <RainStream height={containerSize?.height} />
+      ))}
+    </div>
+  );
+};
+
+export default MatrixRain;
