4658d97edcd368eff322690a7165b2bd562c0b32
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-08-30 00:08:45 +0500

    ✅ Successfull page rendr animation setup

--- Components/Pages-container/pages.js
+++ Components/Pages-container/pages.js
@@ -7,53 +7,29 @@ export default function Pages({ children }, props) {
   useEffect(() => {
     var h = window.innerHeight;
     var w = window.innerWidth;
-    if (props.pageRoute == "work") {
-      //mount
-      console.log(props.pageRoute);
-      var tl = gsap.timeline({ repeat: 0 });
+    //mount
+    console.log(props.pageRoute);
+    var tl = gsap.timeline({ repeat: 0 });
 
-      tl.eventCallback("onComplete", () => {
-        console.log("PAGE TRANSITION  COMPLETED!");
-      });
+    tl.eventCallback("onComplete", () => {
+      console.log("PAGE TRANSITION  COMPLETED!");
+    });
 
-      //Fading In transition
-      tl.fromTo(
-        PagesRef.current,
-        {
-          x: w,
-        },
-        {
-          opacity: 1,
-          x: 0,
-          autoAlpha: 1,
-          duration: 0.5,
-          ease: "back.easeOut",
-        }
-      );
-    } else if (props.pageRoute == null) {
-      //unmount
-      console.log(props.pageRoute);
-      var tl = gsap.timeline({ repeat: 0 });
-
-      tl.eventCallback("onComplete", () => {
-        console.log("PAGE TRANSITION  COMPLETED!");
-      });
-
-      //Fading In transition
-      tl.fromTo(
-        PagesRef.current,
-        {
-          x: 0,
-        },
-        {
-          opacity: 1,
-          x: w,
-          autoAlpha: 1,
-          duration: 0.5,
-        }
-      );
-    }
-  }, [props.pageRoute]);
+    //Fading In transition
+    tl.fromTo(
+      PagesRef.current,
+      {
+        x: w,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        autoAlpha: 1,
+        duration: 0.5,
+        ease: "back.easeOut",
+      }
+    );
+  }, []);
   return (
     <div className={styles.PagesContainer} ref={PagesRef}>
       {children}
