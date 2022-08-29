9271e94d972f6391d3308233f0c02948b3f96962
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-08-30 01:16:18 +0500

    Working on exit animation

--- Components/Pages-container/pages.js
+++ Components/Pages-container/pages.js
@@ -1,37 +1,67 @@
 import { useState, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import styles from "./pages.module.css";
-
+import { useRouter } from "next/router";
 export default function Pages({ children }, props) {
+  const router = useRouter();
+  const [isActive, setIsActive] = useState(false);
   const PagesRef = useRef();
   const landingTransition = () => {
-    var h = window.innerHeight;
-    var w = window.innerWidth;
     //mount
     var tl = gsap.timeline({ repeat: 0 });
-
+    console.log("LANDING ANIMATION STARTED");
     tl.eventCallback("onComplete", () => {
-      console.log("PAGE TRANSITION  COMPLETED!");
+      console.log("LANDING ANIMATION COMPLTED");
+    });
+    //Fading In transition
+    tl.fromTo(
+      PagesRef.current,
+      {
+        xPercent: 100,
+      },
+      {
+        opacity: 1,
+        xPercent: 0,
+        autoAlpha: 1,
+        duration: 1,
+        ease: "back.easeOut",
+      }
+    );
+  };
+  const ExitTransition = () => {
+    //mount
+    var tl = gsap.timeline({ repeat: 0 });
+    console.log("EXIT ANIMATION STARTED");
+    tl.eventCallback("onComplete", () => {
+      console.log("EXIT ANIMATION COMPLETED");
     });
 
     //Fading In transition
     tl.fromTo(
       PagesRef.current,
       {
-        x: w,
+        xPercent: 0,
       },
       {
         opacity: 1,
-        x: 0,
+        xPercent: 100,
         autoAlpha: 1,
-        duration: 0.5,
+        duration: 5,
         ease: "back.easeOut",
       }
     );
   };
   useEffect(() => {
-    landingTransition();
-  });
+    router.events.on("routeChangeStart", landingTransition);
+    // router.events.on("routeChangeComplete", aniEnd);
+    // router.events.on("routeChangeError", aniEnd);
+
+    return () => {
+      router.events.off("routeChangeStart", ExitTransition);
+      router.events.off("routeChangeComplete", ExitTransition);
+      //   router.events.off("routeChangeError", ExitTransition);
+    };
+  }, [router]);
   return (
     <div className={styles.PagesContainer} ref={PagesRef}>
       {children}
