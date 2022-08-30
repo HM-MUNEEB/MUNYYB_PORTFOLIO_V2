464241e2c85422283b20bf57b9aab24f0325470c
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-08-31 02:36:40 +0500

    Added About page

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Munyyb | About</title>
+        <meta
+          name="description"
+          content="Munyyb.com is the the personal portfolio website of Muneeb ur rehman"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        style={{ height: "100vh", width: "100vw", backgroundColor: "black" }}
+      >
+        This is about page
+      </div>
+    </>
+  );
+}
