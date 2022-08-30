a5fd04613efda01c9a54e057c2ed3a1a27cc6a21
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-08-31 02:41:22 +0500

    Added contact-me page

---
+++ pages/contact-me.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Munyyb | Contac-me</title>
+        <meta
+          name="description"
+          content="Munyyb.com is the the personal portfolio website of Muneeb ur rehman"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "white",
+          color: "black",
+        }}
+      >
+        This is Contact-me page
+      </div>
+    </>
+  );
+}
