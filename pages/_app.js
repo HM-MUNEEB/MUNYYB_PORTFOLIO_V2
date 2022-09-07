915091526eb223fe6110aafaf657a726ea87cc38
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-09-08 02:20:51 +0500

    Added console message

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import Container from "../Components/Container/Container";
+import chalk from "chalk";
 
 function MyApp({ Component, pageProps }) {
+  console.log(
+    `%c ${chalk.hex("#00E220").bold("Made with ❤️ by Munyyb Ur Ryhman!")}`,
+    "font-size: 1rem"
+  );
   return (
     <Container>
       <Component {...pageProps} />
